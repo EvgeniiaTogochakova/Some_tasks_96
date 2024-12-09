ad7cee9bf2ea7b19d7d1b124514c9b3759ea5bd9
Jane Togo <lookatma@mail.ru>
2024-12-09 15:59:18 +0700

    Initial commit

---
+++ app/Observers/NewsObserver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\News;
+use Illuminate\Support\Str;
+
+class NewsObserver
+{
+    public function saving(News $news):void{
+        $news->slug=Str::slug($news->title);
+    }
+}
